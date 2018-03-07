72ab6c0d42dbdbbd9d3053e7d22553abb3fc924a
Dorian ROLY <droly@e1r4p15.42.fr>
2018-03-07 16:39:02 +0100

    fat handle in 64 and 32, to check

--- nm.c
+++ nm.c
@@ -6,7 +6,7 @@
 /*   By: droly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/08 11:18:12 by droly             #+#    #+#             */
-/*   Updated: 2018/03/06 17:16:00 by droly            ###   ########.fr       */
+/*   Updated: 2018/03/07 16:38:55 by droly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,26 +60,39 @@ int					nm(char *ptr, off_t sizefile)
 	stru->check = 0;
 	stru->sizefile = sizefile;
 	magic_number = *(int *)ptr;
+	tmpptr = ptr;
 	printf("heymdr\n");
 	if ((unsigned int)magic_number == FAT_CIGAM)
 	{
 		stru->fat_header = (struct fat_header *)ptr;
-//		if (//add si architecture diff || si diff de 64 bits)
 		nbarch = reversebytes32(stru->fat_header->nfat_arch);
+		while (i < nbarch)
+		{
+			stru->fat_arch = ((void*)tmpptr + sizeof(struct fat_header) + (sizeof(struct fat_arch) * i));
+			if (reversebytes32(stru->fat_arch->cputype) == CPU_TYPE_X86_64)
+			{
+				printf("64 bittttt\n");
+				nbarch = i + 1;
+				break;
+			}
+			i++;
+		}
 	}
-	printf("nb arch %d, magic_number : %d\n", nbarch, magic_number);
-	tmpptr = ptr;
+	if (i >= nbarch)
+		i = 0;
+	printf("nb arch %d, magic_number : %x\n", nbarch, magic_number);
 	while (i < nbarch)
 	{
 		magic_number = *(int *)tmpptr;
 		ft_printf("yo, %d\n", i);
-		if ((unsigned int)magic_number == FAT_MAGIC)
-			printf("fat magic\n");
-		else if ((unsigned int)magic_number == FAT_CIGAM)
+		if ((unsigned int)magic_number == FAT_CIGAM || (unsigned int)magic_number == FAT_MAGIC)
 		{
 			stru->fat_header = (struct fat_header *)ptr;
 			stru->fat_arch = ((void*)tmpptr + sizeof(struct fat_header) + (sizeof(struct fat_arch) * i));
-			offset = reversebytes32(stru->fat_arch->offset);
+			if ((unsigned int)magic_number == FAT_CIGAM)
+				offset = reversebytes32(stru->fat_arch->offset);
+			else if ((unsigned int)magic_number == FAT_MAGIC)
+				offset = stru->fat_arch->offset;
 			printf("hehec bon : %d\n", offset);
 			printf("hehec bon2 \n");
 			magic_number = *(int *)(tmpptr + offset);
@@ -87,6 +100,7 @@ int					nm(char *ptr, off_t sizefile)
 		}
 		if ((unsigned int)magic_number == MH_MAGIC_64)
 		{
+			printf("magic64\n");
 			stru->header = (struct mach_header_64 *)ptr;
 			stru->lc = (void *)ptr + sizeof(*stru->header);
 			stru->seg = (struct segment_command_64*)stru->lc;
@@ -97,6 +111,7 @@ int					nm(char *ptr, off_t sizefile)
 //a editer
 		if ((unsigned int)magic_number == MH_CIGAM_64)
 		{
+			printf("cigam64\n");
 			stru->header = (struct mach_header_64 *)ptr;
 			stru->lc = (void *)ptr + sizeof(*stru->header);
 			stru->seg = (struct segment_command_64*)stru->lc;
@@ -106,6 +121,7 @@ int					nm(char *ptr, off_t sizefile)
 		}
 		if ((unsigned int)magic_number == MH_MAGIC)
 		{
+			printf("magic32\n");
 			stru->header32 = (struct mach_header *)ptr;
 			stru->lc = (void *)ptr + sizeof(*stru->header32);
 			stru->seg32 = (struct segment_command*)stru->lc;
@@ -116,13 +132,19 @@ int					nm(char *ptr, off_t sizefile)
 //a editer
 		if ((unsigned int)magic_number == MH_CIGAM)
 		{
-			printf("wesh");
+			printf("cigam32\n");
 			stru->header32 = (struct mach_header *)ptr;
 			stru->lc = (void *)ptr + sizeof(*stru->header32);
 			stru->seg32 = (struct segment_command*)stru->lc;
 			if (checkcorrupt(ptr + stru->sizefile, stru->lc, stru) == 0)
 				return (0);
 			handle_32_reverse(ptr, stru);
+		}
+		if ((unsigned int)magic_number == 0x72613c21)
+		{
+			printf("ARCHIVE\n");
+			stru->sym = (struct symtab_command*)ptr + 8;
+//			printf("size : %d", stru->sym->size);
 		}
 		if (stru->check == 1)
 			return (0);
